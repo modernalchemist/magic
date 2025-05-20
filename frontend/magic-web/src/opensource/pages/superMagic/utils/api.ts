4e7b103276bbdcc52d2ffc9fd606b09086de12fc
Peter_Tan <tanjinjie@kkgroup.cn>
2025-05-20 16:20:02 +0800

    fix: 调整拉取消息接口

--- frontend/magic-web/src/opensource/pages/superMagic/utils/api.ts
+++ frontend/magic-web/src/opensource/pages/superMagic/utils/api.ts
@@ -132,7 +132,7 @@ export const getMessagesByConversationId = async ({
 	page_token?: string
 }): Promise<any> => {
 	return magicClient.post(
-		`/api/v2/magic/conversations/${conversation_id}/message/queries`,
+		`/api/v1/im/conversations/${conversation_id}/messages/queries`,
 		{
 			limit,
 			order,
