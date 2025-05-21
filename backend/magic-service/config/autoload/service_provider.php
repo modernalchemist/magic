481da378640ed78122748bb7a728d980e6a31f9e
lihq1403 <lihqing1403@gmail.com>
2025-05-21 14:49:37 +0800

    fix: update default AES key in service provider configuration

--- backend/magic-service/config/autoload/service_provider.php
+++ backend/magic-service/config/autoload/service_provider.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
  * Copyright (c) The Magic , Distributed under the software license
  */
 return [
-    'model_aes_key' => env('SERVICE_PROVIDER_CONFIG_AES_KEY', 'Q6#iyPSq4EQU'),
+    'model_aes_key' => env('SERVICE_PROVIDER_CONFIG_AES_KEY', ''),
     'office_organization' => env('OFFICE_ORGANIZATION'),
 ];
