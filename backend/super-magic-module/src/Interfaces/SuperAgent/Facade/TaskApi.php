0c6d5eb3747792f1b4de39b4e87152ada310101b
Rock <383329495@qq.com>
2025-07-10 22:28:45 +0800

    feat(SuperAgent): integrate sandbox handling in task processing and enhance error management

--- backend/super-magic-module/src/Interfaces/SuperAgent/Facade/TaskApi.php
+++ backend/super-magic-module/src/Interfaces/SuperAgent/Facade/TaskApi.php
@@ -30,14 +30,12 @@ use Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Request\SaveWorkspaceRequestDTO;
 use Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Request\UserInfoRequestDTO;
 use Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Request\CreateProjectRequestDTO;
 use Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Request\SaveTopicRequestDTO;
-use Dtyq\SuperMagic\Domain\SuperAgent\Service\UserDomainService;
 use Dtyq\SuperMagic\Interfaces\SuperAgent\DTO\Response\CreateTaskApiResponseDTO;
 use Dtyq\SuperMagic\Application\SuperAgent\Service\HandleApiMessageAppService;
 use App\Domain\Contact\Entity\ValueObject\DataIsolation;
 use Dtyq\SuperMagic\Application\SuperAgent\DTO\UserMessageDTO;
 use App\Domain\Contact\Entity\ValueObject\UserType;
 use Dtyq\SuperMagic\Infrastructure\ExternalAPI\SandboxOS\Gateway\Constant\SandboxStatus;
-use Dtyq\SuperMagic\Application\SuperAgent\Service\AgentAppService;
 
 
 #[ApiResponse('low_code')]
