5edaf213e4612b7797968c033c59d3db34807f22
lihq1403 <lihqing1403@gmail.com>
2025-07-16 17:41:29 +0800

    feat(SuperAgent): remove unused dependencies and add mcpConfig property to UserMessageDTO

--- backend/super-magic-module/src/Application/SuperAgent/Service/AgentAppService.php
+++ backend/super-magic-module/src/Application/SuperAgent/Service/AgentAppService.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Dtyq\SuperMagic\Application\SuperAgent\Service;
 
+use App\Domain\Chat\DTO\Message\Common\MessageExtra\SuperAgent\Mention\MentionType;
 use App\Domain\Contact\Entity\ValueObject\DataIsolation;
 use App\Interfaces\Agent\Assembler\FileAssembler;
 use Dtyq\SuperMagic\Domain\SuperAgent\Entity\ValueObject\TaskContext;
@@ -32,6 +33,7 @@ class AgentAppService
     public function __construct(
         LoggerFactory $loggerFactory,
         private SandboxGatewayInterface $gateway,
+        private readonly FileProcessAppService $fileProcessAppService,
         private readonly AgentDomainService $agentDomainService,
     ) {
         $this->logger = $loggerFactory->get('sandbox');
