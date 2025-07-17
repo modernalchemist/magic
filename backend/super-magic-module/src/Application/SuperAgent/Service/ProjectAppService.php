b58fc71aa6f3b136db3fe8bc1a809d21658ce7e3
Rock <383329495@qq.com>
2025-07-17 17:41:36 +0800

    refactor(ProjectAppService, WorkspaceDomainService, SandboxGatewayInterface, SandboxGatewayService, ProjectApi): improve code formatting and consistency across multiple files

--- backend/super-magic-module/src/Application/SuperAgent/Service/ProjectAppService.php
+++ backend/super-magic-module/src/Application/SuperAgent/Service/ProjectAppService.php
@@ -291,10 +291,9 @@ class ProjectAppService extends AbstractAppService
         ];
     }
 
-    public function checkFileListUpdate(RequestContext $requestContext, int $projectId,DataIsolation $dataIsolation): array
+    public function checkFileListUpdate(RequestContext $requestContext, int $projectId, DataIsolation $dataIsolation): array
     {
         $userAuthorization = $requestContext->getUserAuthorization();
-
 
         $projectEntity = $projectEntity = $projectEntity = $this->projectDomainService->getProject($projectId, $userAuthorization->getId());
 
