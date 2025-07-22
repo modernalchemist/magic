d81533bbae9fd3d6ca76a6bda43bd3b5fefe2450
Rock <383329495@qq.com>
2025-07-22 16:58:50 +0800

    refactor(HandleAgentMessageAppService, WorkspaceDomainService): clean up imports and improve method signature consistency

--- backend/super-magic-module/src/Domain/SuperAgent/Service/WorkspaceDomainService.php
+++ backend/super-magic-module/src/Domain/SuperAgent/Service/WorkspaceDomainService.php
@@ -30,8 +30,8 @@ use Dtyq\SuperMagic\ErrorCode\SuperAgentErrorCode;
 use Dtyq\SuperMagic\Infrastructure\ExternalAPI\SandboxOS\Gateway\SandboxGatewayInterface;
 use Dtyq\SuperMagic\Infrastructure\Utils\WorkDirectoryUtil;
 use Exception;
-use RuntimeException;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 use Throwable;
 
 class WorkspaceDomainService
@@ -838,7 +838,7 @@ class WorkspaceDomainService
         return $result;
     }
 
-    public function diffFileListAndVersionFile(array $result, int $projectId,  string $taskId, string $sandboxId, string $organizationCode = ''): bool
+    public function diffFileListAndVersionFile(array $result, int $projectId, string $taskId, string $sandboxId, string $organizationCode = ''): bool
     {
         $dir = '.workspace';
         $workspaceVersion = $this->getWorkspaceVersionByProjectId($projectId, $dir);
