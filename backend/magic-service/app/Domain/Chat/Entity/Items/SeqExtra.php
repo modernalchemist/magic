f08faefa875ec57c5eeddef111a634a1ae4e2a7d
Rock <383329495@qq.com>
2025-07-31 13:59:41 +0800

    feat(MagicChatMessageAppService, SeqExtra): add language property and set language in message extra

--- backend/magic-service/app/Domain/Chat/Entity/Items/SeqExtra.php
+++ backend/magic-service/app/Domain/Chat/Entity/Items/SeqExtra.php
@@ -25,6 +25,9 @@ class SeqExtra extends AbstractEntity
      */
     protected ?int $magicEnvId = null;
 
+
+    protected ?string $language = null;
+
     public function getMagicEnvId(): ?int
     {
         return $this->magicEnvId;
@@ -54,5 +57,16 @@ class SeqExtra extends AbstractEntity
             'magic_env_id' => $this->getMagicEnvId(),
             'topic_id' => $this->getTopicId(),
         ];
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(?string $language): self
+    {
+        $this->language = $language;
+        return $this;
     }
 }
